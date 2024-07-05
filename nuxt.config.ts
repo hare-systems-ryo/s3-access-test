05c0e83bd4dd62fb5318eaa7a9dae29c91911fe0
hare-systems-ryo <ryo@hare-systems.net>
2024-07-05 14:18:32 +0900

    no-message

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,19 @@
 // import hsUi from 'nuxt-hs-ui'
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import twConfig from "./tailwind.config";
+import twConfig from './tailwind.config';
 
 const env = String(process.env.APP_ENV);
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: [
     //
-    ["nuxt-hs-ui", twConfig],
-    ["@nuxtjs/tailwindcss", {}],
-    "@pinia/nuxt",
+    ['nuxt-hs-ui', twConfig],
+    ['@nuxtjs/tailwindcss', {}],
+    '@pinia/nuxt',
     [
-      "@kgierke/nuxt-basic-auth",
+      '@kgierke/nuxt-basic-auth',
       {
-        enabled: env !== "development",
+        enabled: env !== 'development',
         users: (() => {
           const keyPaires: any = [];
           new Array(10).fill(null).forEach((row, index) => {
@@ -30,7 +30,7 @@ export default defineNuxtConfig({
               }
             }
           });
-          console.log("Basic認証", keyPaires);
+          console.log('Basic認証', keyPaires);
           return keyPaires;
         })(),
       },
@@ -38,7 +38,8 @@ export default defineNuxtConfig({
   ],
   devServer: {
     port: 3000, // デフォルト: 3000
-    host: "0.0.0.0", // デフォルト: localhost,
+    host: '0.0.0.0', // デフォルト: localhost,
   },
-  css: ["@/assets/scss/main.scss"],
+  css: ['@/assets/scss/main.scss'],
+  compatibilityDate: '2024-07-05',
 });
